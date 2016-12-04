f9d0bf87a6857a98b590d94c066a37e8eabaa80c
liujinjia@testerlife.com <liujinjia@testerlife.com>
2016-12-05 07:02:15 +0800

    更新第五日的学习资料

--- learning/the_forth_day/sort_list_of_dicts_by_key.py
+++ learning/the_forth_day/sort_list_of_dicts_by_key.py
@@ -8,6 +8,9 @@
         假设你从数据库中检索出来网站会员信息列表，并且以下列的数据结构返回：
 """
 
+from operator import itemgetter
+
+
 ROWS = [
     {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
     {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
@@ -15,3 +18,26 @@ ROWS = [
     {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
 ]
 
+
+def rows_by_fname(arr):
+    """
+        按照arr的fname排序， 并返回一个arr
+    :param arr:
+    :return:
+    """
+    return sorted(arr, key=itemgetter('fname'))
+
+
+def rows_by_uid(arr):
+    """
+        按照arr的uid排序， 并返回一个arr
+    :param arr:
+    :return:
+    """
+    return sorted(arr, key=itemgetter('uid'))
+
+
+if __name__ == '__main__':
+    print(rows_by_fname(ROWS))
+
+    print(rows_by_uid(ROWS))
