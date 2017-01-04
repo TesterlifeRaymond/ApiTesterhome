0be0116d1247d65cd02c9efc9472551ad4aa7601
liujinjia <jinjialiu@creditease.cn>
2017-01-04 20:12:07 +0800

    Merge branch 'master' of /Users/liujinjia/PycharmProjects/LearningPython with conflicts.

---
+++ learning/the_sixth_day.py
@@ -0,0 +1,8 @@
+'''
+File Name       : test_cashApplyBXD
+Description     : 
+Author          :  liujinjia
+change Activity :   
+    create file : the_sixth_day
+    create time : 2016-12-06 11:16
+'''
