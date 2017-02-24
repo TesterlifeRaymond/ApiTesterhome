bbdce3f213b1a4c8c398df60ebd1f0fc1f7a4c5b
liujinjia <jinjialiu@creditease.cn>
2017-02-24 17:01:32 +0800

    test files

---
+++ learning/FeatureForPython/builtin-apply-example-1.py
@@ -0,0 +1,15 @@
+"""
+# -*- coding: utf-8 -*-
+# @Author: liujinjia
+# @Date:   2017-02-19 22:26:25
+# @Last Modified by:   liujinjia
+# @Last Modified time: 2017-02-19 22:31:16
+"""
+
+
+def functions(a, b):
+    """def func and return sum(*args) """
+    return a + b
+
+apply(functions, ("while", "for"))
+""" apply is python2 functions """
